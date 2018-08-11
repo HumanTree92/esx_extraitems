01fb6f3e1fcf5493ec134e865c34a4855e0157b6
William Plunkett Jr <joey.plunkett92@yahoo.com>
2018-08-11 09:14:54 -0500

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -67,20 +67,6 @@ AddEventHandler('esx:onRemoveInventoryItem', function(source, item, count)
 	end
 end)
 
---[[RegisterServerEvent('esx_phone:reload')
-AddEventHandler('esx_phone:reload', function(phoneNumber)
-
-	local _source  = source
-	local xPlayer  = ESX.GetPlayerFromId(_source)
-	local darkPhone = xPlayer.getInventoryItem('black_chip')
-
-	if darkPhone.count > 0 then
-		if xPlayer.job.name ~= 'police' then
-			TriggerEvent('esx_phone:addSource', 'darknet', _source)
-		end
-	end
-end)]]--
-
 -- End of Dark Net
 -- Start of Extra Items
 
