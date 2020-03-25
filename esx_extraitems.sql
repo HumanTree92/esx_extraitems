21e5eaf172cb93fa5602dfa9e8225f95e14958f8
FlawwsX <55849405+FlawwsX@users.noreply.github.com>
2020-03-25 18:57:22 +0000

    Weight to the SQL
    Up to you if you want to add weight to SQL or limit.

--- esx_extraitems.sql
+++ esx_extraitems.sql
@@ -1,4 +1,4 @@
-INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES
+INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES
 	('lockpick', 'Lock Pick', -1, 0, 1),
 	('darknet', 'Dark Net', 1, 0, 1),
 	('drill', 'Drill', 1, 0, 1),
