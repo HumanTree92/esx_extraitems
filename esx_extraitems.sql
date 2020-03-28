637b7149ebc15ff265fcaab6d8bd506182d2af97
William Plunkett Jr <joey.plunkett92@yahoo.com>
2020-03-28 18:14:53 -0500

    Update esx_extraitems.sql

--- esx_extraitems.sql
+++ esx_extraitems.sql
@@ -1,12 +1,12 @@
-INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES
-	('lockpick', 'Lock Pick', -1, 0, 1),
+INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES
+	('lockpick', 'Lock Pick', 1, 0, 1),
 	('darknet', 'Dark Net', 1, 0, 1),
 	('drill', 'Drill', 1, 0, 1),
 	('binoculars', 'Binoculars', 1, 0, 1),
-	('oxygen_mask', 'Oxygen Mask', -1, 0, 1),
-	('bulletproof', 'Bullet-Proof Vest', -1, 0, 1),
-	('firstaidkit', 'First Aid Kit', -1, 0, 1),
-	('clip', 'Weapon Clip', -1, 0, 1)
+	('oxygen_mask', 'Oxygen Mask', 1, 0, 1),
+	('bulletproof', 'Bullet-Proof Vest', 1, 0, 1),
+	('firstaidkit', 'First Aid Kit', 1, 0, 1),
+	('clip', 'Weapon Clip', 1, 0, 1)
 ;
 
 INSERT INTO `shops` (store, item, price) VALUES
